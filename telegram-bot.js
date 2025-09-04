d78241b719cbd4d19c9eba0f47224a9a9db8f5fc
IVAN KOLESNYCHENKO <dev.koleso@gmail.com>
2025-09-04 19:39:07 +0300

    start

---
+++ telegram-bot.js
@@ -0,0 +1,29 @@
+import { Telegraf } from 'telegraf';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+if (!BOT_TOKEN) {
+    throw new Error('TELEGRAM_BOT_TOKEN is not set');
+}
+
+const bot = new Telegraf(BOT_TOKEN);
+
+bot.command('play', (ctx) => {
+    ctx.replyWithGame('d2r');
+});
+
+bot.on('callback_query', (ctx) => {
+    if (ctx.callbackQuery.game_short_name === 'd2r') {
+        ctx.answerCallbackQuery({
+            url: 'https://dota2-roulette.vercel.app/',
+        });
+    } else {
+        ctx.answerCallbackQuery();
+    }
+});
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
