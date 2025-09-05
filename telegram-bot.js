0e5a95f17d9b5cd66c5c9aed2bcbfda29a345ddc
IVAN KOLESNYCHENKO <dev.koleso@gmail.com>
2025-09-05 08:05:41 +0300

    Update telegram-bot.js

--- telegram-bot.js
+++ telegram-bot.js
@@ -11,14 +11,14 @@ const bot = new Telegraf(BOT_TOKEN);
 
 // Кнопка для WebApp (откроет вашу игру внутри Telegram)
 bot.command('play', (ctx) => {
+    const userId = ctx.from.id;
+    const url = `https://dota2-roulette.vercel.app/?user_id=${userId}`;
     ctx.reply('Открыть игру', {
         reply_markup: {
-            keyboard: [
-                [{ text: 'Играть', web_app: { url: 'https://dota2-roulette.vercel.app/' } }]
-            ],
-            resize_keyboard: true,
-            one_time_keyboard: true,
-        },
+            inline_keyboard: [
+                [{ text: 'Играть', url: url }]
+            ]
+        }
     });
 });
 
